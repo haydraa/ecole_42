3691febfb12927baafce48c4c2b7864eef4f95fd
Jalel Eddine Ghribi <jghribi@2D2.42angouleme.fr>
2022-09-26 18:21:17 +0200

    test1

---
+++ project 1/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jghribi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 11:13:44 by jghribi           #+#    #+#             */
+/*   Updated: 2022/09/26 16:51:09 by jghribi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef LBIFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+#include <stdio.h>
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+
+#endif
+
