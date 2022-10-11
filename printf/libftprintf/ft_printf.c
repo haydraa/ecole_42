236e2ccb39cd837938638f51a47684ba23bad8ec
JALEL GHRIBI <ghribijaleleddine@gmail.com>
2022-10-11 16:54:01 +0200

    testtt

--- printf/libftprintf/ft_printf.c
+++ printf/libftprintf/ft_printf.c
@@ -1,54 +1,49 @@
 #include "libftprintf.h"
-
-
+#include <stdio.h>
 void check_format(int argv, ...)
 {
 	va_list arg;
 
-	va_start(arg,argv)
+	va_start(arg,argv);
 	if (arg == 'c')
-		ft_putchar(arg = va_arg(args char));
+		ft_putchar(arg = va_arg(arg, char));
 	if (arg == 's')
-		ft_putstr(arg = va_arg(argc char*));
+		ft_putstr(arg = va_arg(arg, char*));
 	if (arg == 'p')
 		
 	if (arg == 'd')
-		ft_putnbr(arg = va_arg(args int));
+		ft_putnbr(arg = va_arg(arg, int));
 	if (arg == 'i')
-		ft_putnbr(arg = va_arg(args int));
+		ft_putnbr(arg = va_arg(arg, int));
 	if (arg == 'u')
-		ft_putnbr(arg = va_arg(args unsigned int));
+		ft_putnbr(arg = va_arg(arg, unsigned int));
 	if (arg == 'x')
-		ft_putnbr_base(arg = va_arg(args int));
+		ft_putnbr_base(arg = va_arg(arg, int));
 	if (arg == 'X')
-		ft_putnbr_base(arg = va_arg(args int), char X);
+		ft_putnbr_base(arg = va_arg(arg, int));
 	if (arg == '%')
 		write(1 ,'%', 1);
-
-	}
 }
 	
 int ft_prinf(const char *format, ...)
 {
-	va_list parinfo;
+	va_list arg;
 	int i;
 
 	i = 0;
-	va_start (parinfo, format);
-	while (format[i] != '\0')
+	va_start (arg, format);
+	while (*format)
 	{
-		if (format[i] == '%')
-			if(check_format(format[i + 1]) == 1)
-
-				
-
-			
+		if (*format == '%')
+		{	
+			format = ft_check_format(arg, format + 1); 
+		}			
 	}
 	return 1;
 }
 
 int main()
 {
-    const char *s = "sssssssssssss"
-	printf (ft_strlen(s));
+    const char *s = "sssssssssssss";
+	printf(ft_strlen(s));
 }
