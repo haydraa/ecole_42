b4a9d84eb375a8a678b367468ff76cb85f841b01
JALEL <ghribijaleleddine@gmail.com>
2022-11-01 11:19:09 +0100

    test4

--- push_swap/error_case.c
+++ push_swap/error_case.c
@@ -4,28 +4,28 @@ int ft_doubel(char *str, char *s)
 {
 	int i;
 	int j;
-
-	j = 0;
+	
 	i = 0;
-	while(str[i] || s[j])
+	j = 0;
+	while (str[i] || s[j])
 	{
 		if (str[i] != s[j])
 			return 1;
 		i++;
 		j++;
 	}
-	return 0; 
+	return 0;
 }
 
 int ft_check_arg(char *arg)
   {
-         int i;
+        int i;
 
-         i = 0;
-		if (arg[0] = '-')
-				 i = 1;
-		while (arg[i])
+        i = 0;
+		while (arg[i] != '\0')
 		{
+			if (arg[i] == '-')
+					i++;
 			if (arg[i] < '0' || arg[i] > '9')
 				return 0;
 			i++;
@@ -40,31 +40,29 @@ int ft_assembel(int arg, char **arg1)
 
 		i = 1;
 		j = 2;
-		while (i < arg)
+		while (i < arg && arg1[i])
 		{
-				if (ft_check_arg(arg1[i]) == 0)
-				{
-					ft_printf("error\n");
-					return 0;
-				}
-				i++;	
+			if (ft_check_arg(arg1[i]) == 0)
+			{	
+				ft_printf("error\n");
+				return 0;
+			}
+			i++;
 		}
 		i = 1;
 		while (i < arg)
 		{
-				while (j < arg && arg1[j])
+			j = 2;
+			while (j < arg)
+			{
+				if (ft_doubel(arg1[i],arg1[j]) == 0)
 				{
-			//		if (j = i)
-			//			j++;
-					if (ft_doubel(arg1[i] ,arg1[j]) == 1)
-					{
-						ft_printf("error\n");
-						return 0;
-					}
-					j++;
+					ft_printf("error\n");
+					return 0;
 				}
-				j = 1;
-				i++;
+				j++;
+			}
+			i++;
 		}
 		return 0;
 }
